c78ac74f4023c2d81f2166e86d44752d2158d6ab
souppower <ryo.kubota@globis.co.jp>
2018-07-08 23:36:37 +0900

    impl feed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
-// 2
+let idCount = links.length;
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (root, args) => {
+        const link = {
+            id: `link-${idCount++}`,
+            description: args.description,
+            url: args.url,
+        }
+        links.push(link)
+        return link
+    }
   }
 }
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
